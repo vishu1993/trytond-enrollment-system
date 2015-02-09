63c374ce0345d45e1e2771cfba2581e600906734
vishu jain <vishu@vishu-Vostro-3446.(none)>
2015-02-09 14:45:49 +0530

    Added Personal Details to enrollment system #6944

--- __init__.py
+++ __init__.py
@@ -7,10 +7,12 @@
 """
 from trytond.pool import Pool
 from medical_family_details import Party
+from enrollment_system import PartyPersonalDetails
 
 
 def register():
     Pool.register(
         Party,
+        PartyPersonalDetails,
         module='enrollment_system', type_='model'
     )
